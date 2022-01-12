957c245f2a7644895701afd7e51664b1ac7a708b
KrystianJc <jackiewicz.krystian4@gmail.com>
2022-01-12 14:45:04 +0100

    Nadchodzi plik

--- DziennikWykonanyPrzezKrystianJackiewicz/src/main/java/jackiewicz/krystian/dziennikwykonanyprzezkrystianjackiewicz/DziennikWykonanyPrzezKrystianJackiewicz.java
+++ DziennikWykonanyPrzezKrystianJackiewicz/src/main/java/jackiewicz/krystian/dziennikwykonanyprzezkrystianjackiewicz/DziennikWykonanyPrzezKrystianJackiewicz.java
@@ -26,17 +26,123 @@ public class DziennikWykonanyPrzezKrystianJackiewicz extends javax.swing.JFrame 
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jNameTextField = new javax.swing.JLabel();
+        jSurnameTextField = new javax.swing.JLabel();
+        jClass = new javax.swing.JLabel();
+        jLesson = new javax.swing.JLabel();
+        jGrade = new javax.swing.JLabel();
+        jAllTextField = new javax.swing.JLabel();
+        jName = new javax.swing.JTextField();
+        jSurname = new javax.swing.JTextField();
+        jComboClass = new javax.swing.JComboBox<>();
+        jComboLesson = new javax.swing.JComboBox<>();
+        jSliderGrades = new javax.swing.JSlider();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        jNameTextField.setFont(new java.awt.Font("Dialog", 1, 11)); // NOI18N
+        jNameTextField.setText("Podaj swoje imię:");
+
+        jSurnameTextField.setFont(new java.awt.Font("Dialog", 1, 11)); // NOI18N
+        jSurnameTextField.setText("Podaj swoje nazwisko:");
+
+        jClass.setFont(new java.awt.Font("Dialog", 1, 11)); // NOI18N
+        jClass.setText("Klasa");
+
+        jLesson.setFont(new java.awt.Font("Dialog", 1, 11)); // NOI18N
+        jLesson.setText("Przedmiot");
+
+        jGrade.setFont(new java.awt.Font("Dialog", 1, 11)); // NOI18N
+        jGrade.setText("Ocena");
+
+        jAllTextField.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        jAllTextField.setText("Wszystkie wpisy");
+
+        jComboClass.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "2PTN", "2FTN", "2ALN", "2", " " }));
+
+        jComboLesson.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Matematyka", " " }));
+
+        jSliderGrades.setMajorTickSpacing(1);
+        jSliderGrades.setMaximum(6);
+        jSliderGrades.setMinorTickSpacing(1);
+        jSliderGrades.setPaintLabels(true);
+        jSliderGrades.setPaintTicks(true);
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jSurnameTextField)
+                            .addComponent(jNameTextField)
+                            .addComponent(jGrade))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGap(62, 62, 62)
+                                .addComponent(jSurname))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jSliderGrades, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jName, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLesson)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jComboLesson, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jClass)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jComboClass, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(59, 59, 59)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jAllTextField)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 409, Short.MAX_VALUE)
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(41, 41, 41)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jNameTextField)
+                    .addComponent(jAllTextField)
+                    .addComponent(jName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(68, 68, 68)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jSurnameTextField)
+                            .addComponent(jSurname, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(61, 61, 61)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jClass)
+                            .addComponent(jComboClass, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 88, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jComboLesson, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLesson))
+                        .addGap(94, 94, 94)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jGrade)
+                            .addComponent(jSliderGrades, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jScrollPane1)))
+                .addGap(121, 121, 121))
         );
 
         pack();
@@ -78,5 +184,18 @@ public class DziennikWykonanyPrzezKrystianJackiewicz extends javax.swing.JFrame 
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jAllTextField;
+    private javax.swing.JLabel jClass;
+    private javax.swing.JComboBox<String> jComboClass;
+    private javax.swing.JComboBox<String> jComboLesson;
+    private javax.swing.JLabel jGrade;
+    private javax.swing.JLabel jLesson;
+    private javax.swing.JTextField jName;
+    private javax.swing.JLabel jNameTextField;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JSlider jSliderGrades;
+    private javax.swing.JTextField jSurname;
+    private javax.swing.JLabel jSurnameTextField;
+    private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
 }
